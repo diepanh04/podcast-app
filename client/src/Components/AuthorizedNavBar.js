cfed748e70b1fce1a85501adb85b93188c95ae77
Ashley Nguyen <88578410+diepanh04@users.noreply.github.com>
2023-06-22 17:36:01 -0400

    add authenticateUser action in redux

--- client/src/Components/AuthorizedNavBar.js
+++ client/src/Components/AuthorizedNavBar.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import logo from '../Assets/Images/Podcasity.png';
+import Button from '@mui/material/Button';
+import firebase from '../services/firebase';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { authenticateUser } from '../actions/auth.js';
+import AvatarButton from './AvatarButton';
+import logo from '../Assets/Images/Podcasity.png';
 
 const AuthorizedNavBar = ({ children }) => {
+  const dispatch = useDispatch();
+
+  const logOut = async () => {
+    await firebase.auth.signOut();
+  }
+
+  useEffect(() => {
+    dispatch(authenticateUser())
+  }, [dispatch]);
+
+  const currUser = useSelector(state => state.auth.currUser);
+
   return (
     <div>
       <Grid container justifyContent="space-between" alignItems="center">
@@ -13,8 +31,11 @@ const AuthorizedNavBar = ({ children }) => {
             <img src={logo} style={{ height: '12vh' }} alt="Logo" />
           </Link>
         </Grid>
-        <Grid item>
-          <Typography>logged in</Typography>
+        <Grid item sx={{ display: 'flex', alignItems: 'center' }}>
+          <AvatarButton photo={currUser.image} name={currUser.name}/>
+          <Button onClick={logOut}>
+            Log Out
+          </Button>
         </Grid>
       </Grid>
       {children}
--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -2,4 +2,5 @@ import axios from 'axios';
 
 const url = 'http://localhost:4000/users';
 
-export const register = (newUser) => axios.post(`${url}/register`, newUser);  
+export const register = (newUser) => axios.post(`${url}/register`, newUser);
+export const authenticateUser = (headers) => axios.get(`${url}`, headers);
